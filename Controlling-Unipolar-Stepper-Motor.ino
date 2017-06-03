6eecafa1db311591ef971113c7fb1498e80ca582
Emanuel Suriano <emanuel.suriano@fdvsolutions.com>
2017-06-03 19:51:38 -0300

    Add new library - HashMap!

--- Controlling-Unipolar-Stepper-Motor.ino
+++ Controlling-Unipolar-Stepper-Motor.ino
@@ -1,27 +1,35 @@
-//Pin connected to ST_CP of 74HC595
-int latchPin = 8;
-//Pin connected to SH_CP of 74HC595
-int clockPin = 12;
-////Pin connected to DS of 74HC595
-int dataPin = 11;
+#include <HashMap.h>
 
-const int dirPin = 4;
+// DEFINITION_VARIABLES
+const byte HASH_SIZE = 24; //define the max size of the hashtable
+HashType<char,int> hashRawArray[HASH_SIZE]; //storage 
+HashMap<char,int> lettersMap = HashMap<char,int>( hashRawArray , HASH_SIZE ); //handles the storage [search,retrieve,insert]
 
-const int right = HIGH;
-const int left = LOW;
+const int latchPin = 8; //Pin connected to ST_CP of 74HC595
+const int clockPin = 12; //Pin connected to SH_CP of 74HC595
+const int dataPin = 11; ////Pin connected to DS of 74HC595
 
+const int dirPin = 4; ////Pin that control the direction
+const int RIGHT_DIRECTION = HIGH;
+const int LEFT_DIRECTION = LOW;
+
+// ARDUINO_LIFE_CYCLES
 void setup() {
+  //setup hashmap
+  setupLettersMap();
+
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(dirPin,OUTPUT);
+  hashMap.debug();
 }
 
 void loop() {
-  digitalWrite(dirPin, right);
+  digitalWrite(dirPin, RIGHT_DIRECTION);
   digitalWrite(latchPin, LOW);
   
-  shiftOut(dataPin, clockPin, MSBFIRST, 0b11111000);  
+  shiftOut(dataPin, clockPin, MSBFIRST, hashMap.getValueOf('a'));  
   
   digitalWrite(latchPin, HIGH);
   digitalWrite(latchPin, LOW);
@@ -30,4 +38,11 @@ void loop() {
   
   digitalWrite(latchPin, HIGH);
   delay(50);
+}
+
+// HELPER_FUNCTIONS
+void setupLettersMap() {
+  lettersMap[0]('a', 0b10000000);
+  lettersMap[1]('b', 0b11000000);
+  // complete with the all the letters that are missing ..
 }
