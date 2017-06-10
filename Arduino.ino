11409e64049304c40492110b0c6c8a716cb41e46
Emanuel <emanuel.suriano@gmail.com>
2017-06-10 15:29:23 -0300

    Using array method instead of map

--- Arduino.ino
+++ Arduino.ino
@@ -1,8 +1,3 @@
-#include <HashMap.h>
-
-// DEFINITION_VARIABLES
-HashMap<char,int> lettersMap;
-
 const int LATCH_PIN = 8; //Pin connected to ST_CP of 74HC595
 const int CLOCK_PIN = 12; //Pin connected to SH_CP of 74HC595
 const int DATA_PIN = 11; ////Pin connected to DS of 74HC595
@@ -11,89 +6,71 @@ const int DIRECTION_PIN = 4; ////Pin that control the direction
 const int RIGHT_DIRECTION = HIGH;
 const int LEFT_DIRECTION = LOW;
 
+const int DELAY_BETWEEN_LETTERS = 1000;
+const char letters[5] = {'A', 'E', 'S', 'Z', 'Y'};
+
 // ARDUINO_LIFE_CYCLES
 void setup() {
-  //setup hashmap
-  lettersMap = getAlphabetMap();
-
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
   pinMode(DIRECTION_PIN, OUTPUT);
-  hashMap.debug();
 }
 
-// void loop() {
-//   digitalWrite(DIRECTION_PIN, RIGHT_DIRECTION);
-//   digitalWrite(LATCH_PIN, LOW);
-  
-//   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, hashMap.getValueOf('A'));  
-  
-//   digitalWrite(LATCH_PIN, HIGH);
-//   digitalWrite(LATCH_PIN, LOW);
-  
-//   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);  
-  
-//   digitalWrite(LATCH_PIN, HIGH);
-//   delay(50);
-// }
-
-char letters[5] = {'A', 'E', 'S', 'Z', 'Y'};
-
 void loop() {
-  for(int i=0; i< 5; i++){
-      representLetter(letters[i]);
-      delay(100);
+  digitalWrite(DIRECTION_PIN, RIGHT_DIRECTION); // Set direction of the 
+  int arrayLenght = sizeof(letters)/sizeof(letters[0]);
+  for(int i=0; i< arrayLenght; i++){
+      representLetter(letters[i], 3);
+      delay(DELAY_BETWEEN_LETTERS);
   }
 } 
 
-void representLetter(char letter) {
-  digitalWrite(DIRECTION_PIN, RIGHT_DIRECTION);
-  digitalWrite(LATCH_PIN, LOW);
-  
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, letter);
-  
-  digitalWrite(LATCH_PIN, HIGH);
-  digitalWrite(LATCH_PIN, LOW);
-  
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);  
-  
-  digitalWrite(LATCH_PIN, HIGH);
-  delay(50);
+void representLetter(char letter, int numberOfLoops) {
+  for(int i=0; i < numberOfLoops * 20; i++){
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, getCodeOfLetter(letter));
+    digitalWrite(LATCH_PIN, HIGH);
+
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
+    digitalWrite(LATCH_PIN, HIGH);
+    delay(10);   
+  }
 }
 
-// HELPER_FUNCTIONS
-HashMap<char,int> getAlphabetMap() {
-  const byte HASH_SIZE = 25; //define the max size of the hashtable
-  HashType<char,int> hashRawArray[HASH_SIZE]; //storage 
-  const result = HashMap<char,int>( hashRawArray , HASH_SIZE ); //handles the storage [search,retrieve,insert]
-
-  result[0]('A',  0b10000000);
-  result[1]('B',  0b11000000);
-  result[2]('C',  0b10010000);
-  result[3]('D',  0b10011000);
-  result[4]('E',  0b10001000);
-  result[5]('F',  0b11010000);
-  result[6]('G',  0b11011000);
-  result[7]('H',  0b11001000);
-  result[8]('I',  0b01001000);
-  result[9]('J',  0b01011000);
-  result[10]('K', 0b10100000);
-  result[11]('L', 0b11100000);
-  result[12]('M', 0b10110000);
-  result[13]('N', 0b10111000);
-  result[14]('O', 0b10101000);
-  result[15]('P', 0b11110000);
-  result[16]('Q', 0b11111000);
-  result[17]('R', 0b11101000);
-  result[18]('S', 0b01110000);
-  result[19]('T', 0b01111000);
-  result[20]('U', 0b10100100);
-  result[21]('V', 0b11100100);
-  result[22]('W', 0b01011100);
-  result[23]('X', 0b10110100);
-  result[24]('Y', 0b10111100);
-  result[25]('Z', 0b10101100);
-
-  return result;
+int getCodeOfLetter(char letter) {
+  switch(letter) {
+    case 'A': return 0b10000000;
+    case 'B': return 0b11000000;
+    case 'C': return 0b10010000;
+    case 'D': return 0b10011000;
+    case 'E': return 0b10001000;
+    case 'F': return 0b11010000;
+    case 'G': return 0b11011000;
+    case 'H': return 0b11001000;
+    case 'I': return 0b01001000;
+    case 'J': return 0b01011000;
+    case 'K': return 0b10100000;
+    case 'L': return 0b11100000;
+    case 'M': return 0b10110000;
+    case 'N': return 0b10111000;
+    case 'O': return 0b10101000;
+    case 'P': return 0b11110000;
+    case 'Q': return 0b11111000;
+    case 'R': return 0b11101000;
+    case 'S': return 0b01110000;
+    case 'T': return 0b01111000;
+    case 'U': return 0b10100100;
+    case 'V': return 0b11100100;
+    case 'W': return 0b01011100;
+    case 'X': return 0b10110100;
+    case 'Y': return 0b10111100;
+    case 'Z': return 0b10101100;
+    default: return 0;
+  }
 }
+
+
+
+
