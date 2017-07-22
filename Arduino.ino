ba521f719f40dae9fd91340875ff8e7a0254c1a4
Emanuel Suriano <emanuel.suriano@intive-fdv.com>
2017-07-21 23:41:14 -0300

    Refactoring lot of code, creating new functions in order to make the code more readable, adding a few helper functions, and adding a TODO stuff

--- Arduino.ino
+++ Arduino.ino
@@ -1,18 +1,29 @@
-const int LATCH_PIN = 8; //Pin connected to ST_CP of 74HC595
-const int CLOCK_PIN = 12; //Pin connected to SH_CP of 74HC595
-const int DATA_PIN = 11; ////Pin connected to DS of 74HC595
+#define LENGTH_ARRAY(array) (sizeof(array) / sizeof((array)[0]))
 
+// PINS_CONNECIONS
+const int LATCH_PIN = 8;     //Pin connected to ST_CP of 74HC595
+const int CLOCK_PIN = 12;    //Pin connected to SH_CP of 74HC595
+const int DATA_PIN = 11;     ////Pin connected to DS of 74HC595
 const int DIRECTION_PIN = 4; ////Pin that control the direction
+
+// DELAYS
+const int DELAY_BETWEEN_WORDS = 2000;
+const int DELAY_BETWEEN_MOTOR_STEPS = 10;
+
+// MOTOR_DIRECTIONS
 const int RIGHT_DIRECTION = HIGH;
 const int LEFT_DIRECTION = LOW;
 
-const int DELAY_BETWEEN_LETTERS = 2000;
-const char letters[5] = {'A', 'A', 'A', 'A', 'A'};
+// MOTORS_VARIABLES
 const int NUMBER_OF_STEPS_PER_LOOP = 20;
+
 const char EMPTY_CHARACTER = '@';
+char lastWord[4] = {EMPTY_CHARACTER, EMPTY_CHARACTER, EMPTY_CHARACTER, EMPTY_CHARACTER};
+char *words[4] = {"A", "AB", "ABC", "ABCD"};
 
 // ARDUINO_LIFE_CYCLES
-void setup() {
+void setup()
+{
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
@@ -20,182 +31,259 @@ void setup() {
   Serial.begin(9600);
 }
 
-char lastCharacter = EMPTY_CHARACTER;
+/* TODO: please create a library in order to remove a lot of code inside here,
+  remember that this file has to manage only the life cycle of the arduino
 
-void loop() {
-  for(int i=0; i < 5; i++){
-    if(lastCharacter != EMPTY_CHARACTER) {
-      representLetter(lastCharacter, LEFT_DIRECTION);  
-    }
-    
-    representLetter('A', RIGHT_DIRECTION);
-    delay(DELAY_BETWEEN_LETTERS);  
+  In my opinion, I will create 3 files more:
+    - Configuration: manage all the pins, motors, and other configuration that we have.
+    - Utils: store our helper functions, such as LENGHT_ARRAY, also I will love to have a function that can print more than one value.
+    - [INSERT_LIBRARY_NAME_HERE]: will have only one public method --> representWord
+        - Also it can have a constructor that will take the ammount of letters!
+*/
+
+void loop()
+{
+  int length = LENGTH_ARRAY(words);
+  for (int i = 0; i < length; i++)
+  {
+    representWord(words[i]);
+    delay(DELAY_BETWEEN_WORDS);
+  }
+
+  Serial.println("End of printing words!");
+}
+
+void representWord(char *word)
+{
+  Serial.print("Printing word: ");
+  Serial.println(word);
+  int len = strlen(word);
+  for (int i = 0; i < len; i++)
+  {
+    representLetter(i, word[i]);
   }
 }
 
-void representLetter(char letter, int dir) {
+void representLetter(int letterPosition, char letter)
+{
+  if (lastWord[letterPosition] == EMPTY_CHARACTER)
+  {
+    spinMotor(letterPosition, lastWord[letterPosition], LEFT_DIRECTION);
+  }
+
+  spinMotor(letterPosition, letter, RIGHT_DIRECTION);
+  lastWord[letterPosition] = letter;
+}
+
+const int multiplexorRemoval[4][2] = {
+    {0b01111111, 0b10111111},
+    {0b11011111, 0b11101111},
+    {0b01110111, 0b01111011},
+    {0b01111101, 0b01111110},
+};
+
+void spinMotor(int letterPosition, char letter, int dir)
+{
   digitalWrite(DIRECTION_PIN, dir);
-  
-  lastCharacter = letter;
-  int * stepsPerMotor = getStepsPerMotor(letter);
-  int spinningMotor =  0b11111111;
-  for(int i=0; i < NUMBER_OF_STEPS_PER_LOOP; i++){
-    digitalWrite(LATCH_PIN, LOW);
-    
-    if (stepsPerMotor[0] == i) {
-      spinningMotor = spinningMotor & 0b01111111;
-    }
-    if (stepsPerMotor[1] == i) {
-      spinningMotor = spinningMotor & 0b10111111;
-    }
-    
-    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, spinningMotor);
-    digitalWrite(LATCH_PIN, HIGH);
-
-    digitalWrite(LATCH_PIN, LOW);
-    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
-    digitalWrite(LATCH_PIN, HIGH);
-    delay(10);
+  int *stepsPerMotor = getStepsPerMotor(letter);
+  for (int i = 0; i < NUMBER_OF_STEPS_PER_LOOP; i++)
+  {
+    int multiplexorData = getMultiplexorData(letterPosition, stepsPerMotor, i);
+    sendMultiplexorData(multiplexorData);
+    delay(DELAY_BETWEEN_MOTOR_STEPS);
   }
 }
 
-int stepsPerMotor[2];
-int * getStepsPerMotor(char letter) {
-  switch(letter) {
-    case 'A': {
-      stepsPerMotor[0] = 6;
-      stepsPerMotor[1] = 0;
-      break;
+int getMultiplexorData(int letterPosition, int *stepsPerMotor, int lap)
+{
+  int multiplexorData = 0b11111111;
+  for (int j = 0; j < 2; j++)
+  {
+    if (stepsPerMotor[j] >= lap)
+    {
+      multiplexorData = multiplexorData & multiplexorRemoval[letterPosition][j];
     }
-    case 'B': {
-      stepsPerMotor[0] = 10;
-      stepsPerMotor[1] = 0;
-      break;
-    }
-    case 'C': {
-      stepsPerMotor[0] = 6;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'D': {
-      stepsPerMotor[0] = 6;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'E': {
-      stepsPerMotor[0] = 6;
-      stepsPerMotor[1] = 8;
-      break;
-    }
-    case 'F': {
-      stepsPerMotor[0] = 10;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'G': {
-      stepsPerMotor[0] = 10;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'H': {
-      stepsPerMotor[0] = 10;
-      stepsPerMotor[1] = 8;
-      break;
-    }
-    case 'I': {
-      stepsPerMotor[0] = 8;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'J': {
-      stepsPerMotor[0] = 8;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'K': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 0;
-      break;
-    }
-    case 'L': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 0;
-      break;
-    }
-    case 'M': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'N': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'O': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 8;
-      break;
-    }
-    case 'P': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'Q': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'R': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 8;
-      break;
-    }
-    case 'S': {
-      stepsPerMotor[0] = 16;
-      stepsPerMotor[1] = 12;
-      break;
-    }
-    case 'T': {
-      stepsPerMotor[0] = 16;
-      stepsPerMotor[1] = 16;
-      break;
-    }
-    case 'U': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 6;
-      break;
-    }
-    case 'V': {
-      stepsPerMotor[0] = 18;
-      stepsPerMotor[1] = 6;
-      break;
-    }
-    case 'W': {
-      stepsPerMotor[0] = 8;
-      stepsPerMotor[1] = 18;
-      break;
-    }
-    case 'X': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 14;
-      break;
-    }
-    case 'Y': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 18;
-      break;
-    }
-    case 'Z': {
-      stepsPerMotor[0] = 14;
-      stepsPerMotor[1] = 10;
-      break;
-    }
-    default: {
-      stepsPerMotor[0] = 0;
-      stepsPerMotor[1] = 0;
-    }
+  }
+  return multiplexorData;
+}
+
+void sendMultiplexorData(int data)
+{
+  digitalWrite(LATCH_PIN, LOW);
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data);
+  digitalWrite(LATCH_PIN, HIGH);
+
+  digitalWrite(LATCH_PIN, LOW);
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
+  digitalWrite(LATCH_PIN, HIGH);
+}
+
+int *getStepsPerMotor(char letter)
+{
+  static int stepsPerMotor[2];
+  switch (letter)
+  {
+  case 'A':
+  {
+    stepsPerMotor[0] = 6;
+    stepsPerMotor[1] = 0;
+    break;
+  }
+  case 'B':
+  {
+    stepsPerMotor[0] = 10;
+    stepsPerMotor[1] = 0;
+    break;
+  }
+  case 'C':
+  {
+    stepsPerMotor[0] = 6;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'D':
+  {
+    stepsPerMotor[0] = 6;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'E':
+  {
+    stepsPerMotor[0] = 6;
+    stepsPerMotor[1] = 8;
+    break;
+  }
+  case 'F':
+  {
+    stepsPerMotor[0] = 10;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'G':
+  {
+    stepsPerMotor[0] = 10;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'H':
+  {
+    stepsPerMotor[0] = 10;
+    stepsPerMotor[1] = 8;
+    break;
+  }
+  case 'I':
+  {
+    stepsPerMotor[0] = 8;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'J':
+  {
+    stepsPerMotor[0] = 8;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'K':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 0;
+    break;
+  }
+  case 'L':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 0;
+    break;
+  }
+  case 'M':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'N':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'O':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 8;
+    break;
+  }
+  case 'P':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'Q':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'R':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 8;
+    break;
+  }
+  case 'S':
+  {
+    stepsPerMotor[0] = 16;
+    stepsPerMotor[1] = 12;
+    break;
+  }
+  case 'T':
+  {
+    stepsPerMotor[0] = 16;
+    stepsPerMotor[1] = 16;
+    break;
+  }
+  case 'U':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 6;
+    break;
+  }
+  case 'V':
+  {
+    stepsPerMotor[0] = 18;
+    stepsPerMotor[1] = 6;
+    break;
+  }
+  case 'W':
+  {
+    stepsPerMotor[0] = 8;
+    stepsPerMotor[1] = 18;
+    break;
+  }
+  case 'X':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 14;
+    break;
+  }
+  case 'Y':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 18;
+    break;
+  }
+  case 'Z':
+  {
+    stepsPerMotor[0] = 14;
+    stepsPerMotor[1] = 10;
+    break;
+  }
+  default:
+  {
+    stepsPerMotor[0] = 0;
+    stepsPerMotor[1] = 0;
+  }
   }
   return stepsPerMotor;
 }
