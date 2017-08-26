8ea478600b224984fbc6dd709251bffeaf689b85
Emanuel Suriano <emanuel.suriano@intive-fdv.com>
2017-08-25 22:37:40 -0300

    So by now, this is working with this configuration. The next thing to do is clean up this shit :)

--- Arduino.ino
+++ Arduino.ino
@@ -2,8 +2,9 @@
 
 // PINS_CONNECIONS
 const int DIRECTION_PIN = 11; ////Pin that control the direction
-const int LATCH_PIN = 10;     //Pin connected to ST_CP of 74HC595
-const int CLOCK_PIN = 9;      //Pin connected to SH_CP of 74HC595
+
+const int CLOCK_PIN = 10; //Pin connected to SH_CP of 74HC595
+const int LATCH_PIN = 9;  //Pin connected to ST_CP of 74HC595
 const int DATA_PIN = 8;       ////Pin connected to DS of 74HC595
 
 // DELAYS
@@ -44,6 +45,57 @@ void setup()
         - Also it can have a constructor that will take the ammount of letters!
 */
 
+// the heart of the program
+void myShiftOut(int myDataPin, int myClockPin, byte myDataOut)
+{
+  // This shifts 8 bits out MSB first,
+  //on the rising edge of the clock,
+  //clock idles low
+
+  //internal function setup
+  int i = 0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  //clear everything out just in case to
+  //prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+
+  //for each bit in the byte myDataOut�
+  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  //This means that %00000001 or "1" will go through such
+  //that it will be pin Q0 that lights.
+  for (i = 7; i >= 0; i--)
+  {
+    digitalWrite(myClockPin, 0);
+
+    //if the value passed to myDataOut and a bitmask result
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000
+    // and proceeds to set pinState to 1.
+    if (myDataOut & (1 << i))
+    {
+      pinState = 1;
+    }
+    else
+    {
+      pinState = 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0);
+}
+
 void loop()
 {
   int length = LENGTH_ARRAY(words);
@@ -69,8 +121,11 @@ void representWord(char *word)
       
     //sendMultiplexorData(multiplexorData);
     //sendMultiplexorData(0b00000001); // so by doing this, only one motor should spin
-    sendMultiplexorData(0); // so by doing this, only one motor should spin
-    delay(DELAY_BETWEEN_MOTOR_STEPS);
+    // sendMultiplexorData(0b11110000); // so by doing this, only one motor should spin
+    // sendMultiplexorData(0b00000011); // so by doing this, only one motor should spin
+    
+//    delay(DELAY_BETWEEN_MOTOR_STEPS);
+
   }
 }
 
@@ -102,12 +157,18 @@ int getMultiplexorData(char *word, int step)
 void sendMultiplexorData(int data)
 {
   digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data);
+  // myShiftOut(DATA_PIN, CLOCK_PIN, data);
+  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, data);   
+  // shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data);
   digitalWrite(LATCH_PIN, HIGH);
+  delay(10);
 
   digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
+  // myShiftOut(DATA_PIN, CLOCK_PIN, 0);
+  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0);   
+  // shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data);
   digitalWrite(LATCH_PIN, HIGH);
+  delay(10);
 }
 
 int *getStepsPerMotor(char letter)
@@ -279,3 +340,5 @@ int *getStepsPerMotor(char letter)
   }
   return stepsPerMotor;
 }
+
+
