53205f1f9820765457b925879ee22d55bbb543d7
Emanuel Suriano <emanuel.suriano@intive-fdv.com>
2017-09-23 01:09:29 -0300

    Formating code

--- BarrelArranger.cpp
+++ BarrelArranger.cpp
@@ -24,40 +24,45 @@ const int MULTIPLEXOR_DATA_BY_MOTOR_NUMBER[8] = {
     0b00000001,
 };
 
-
 BarrelArranger::BarrelArranger(MultiplexorHandler &multiplexorHandler, int directionPin) : _multiplexorHandler(multiplexorHandler)
 {
-  pinMode(directionPin, OUTPUT);
-  digitalWrite(directionPin, RIGHT_DIRECTION);
-    
-  _directionPin = directionPin;
-  _lastDirection = RIGHT_DIRECTION;
+    pinMode(directionPin, OUTPUT);
+    digitalWrite(directionPin, RIGHT_DIRECTION);
+
+    _directionPin = directionPin;
+    _lastDirection = RIGHT_DIRECTION;
 }
 
-bool BarrelArranger::hasToMoveBarrel(char* word) {
+bool BarrelArranger::hasToMoveBarrel(char *word)
+{
     return word[0] == MOVE_BARREL_CHARACTER;
 }
 
 // expected shape of word when moving a barrel --> -D1 (D is the corresponding direction and 1 is the motor number)
-void BarrelArranger::moveBarrel(char * word){
+void BarrelArranger::moveBarrel(char *word)
+{
     int direction = word[1] == 'D' ? RIGHT_DIRECTION : LEFT_DIRECTION;
     int motorNumber = (int)word[2] - 48;
 
     moveMotor(motorNumber, direction);
 }
 
-void BarrelArranger::moveMotor(int motorNumber, int direction) {
-    if(_lastDirection != direction) {
+void BarrelArranger::moveMotor(int motorNumber, int direction)
+{
+    if (_lastDirection != direction)
+    {
         digitalWrite(_directionPin, direction);
     }
 
     _lastDirection = direction;
 
-    for (int side = 0; side < STEPS_PER_MOVE; side++) {
+    for (int side = 0; side < STEPS_PER_MOVE; side++)
+    {
         _multiplexorHandler.sendMultiplexorData(MULTIPLEXOR_DATA_BY_MOTOR_NUMBER[motorNumber]);
     }
 
-    if(direction == LEFT_DIRECTION) {
+    if (direction == LEFT_DIRECTION)
+    {
         digitalWrite(_directionPin, RIGHT_DIRECTION);
     }
 }
