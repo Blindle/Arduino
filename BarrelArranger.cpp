2a5f57f6d480a57efed8ba3c4dade79d4ca287aa
Emanuel Suriano <emanuel.suriano@intive-fdv.com>
2017-09-23 00:35:54 -0300

    Adding new files

---
+++ BarrelArranger.cpp
@@ -0,0 +1,74 @@
+/*
+  BarrelArranger.h - Library for manualy moving an specific to the right or to the left
+  Created by Blindle, 22 Sept, 2017.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "BarrelArranger.h"
+#include "MultiplexorHandler.h"
+
+const char MOVE_BARREL_CHARACTER = '-';
+const int STEPS_PER_MOVE = 100;
+const int LEFT_DIRECTION = LOW;
+const int RIGHT_DIRECTION = HIGH;
+
+const int MULTIPLEXOR_DATA_BY_MOTOR_NUMBER[8] = {
+    0b10000000,
+    0b01000000,
+    0b00100000,
+    0b00010000,
+    0b00001000,
+    0b00000100,
+    0b00000010,
+    0b00000001,
+};
+
+
+BarrelArranger::BarrelArranger(MultiplexorHandler &multiplexorHandler, int directionPin)
+{
+//   pinMode(latchPin, OUTPUT);
+//   pinMode(clockPin, OUTPUT);
+//   pinMode(dataPin, OUTPUT);
+
+  pinMode(directionPin, OUTPUT);
+  digitalWrite(directionPin, RIGHT_DIRECTION);
+//   _latchPin = latchPin;
+//   _clockPin = clockPin;
+//   _dataPin = dataPin;
+    _multiplexorHandler = multiplexorHandler;
+  _directionPin = directionPin;
+  _lastDirection = RIGHT_DIRECTION;
+  
+}
+
+
+// digitalWrite(DIRECTION_PIN, HIGH);
+
+bool BarrelArranger::hasToMoveBarrel(char* word) {
+    return word[0] == MOVE_BARREL_CHARACTER;
+}
+
+// expected shape of word when moving a barrel --> -D1 (D is the corresponding direction and 1 is the motor number)
+void BarrelArranger::moveBarrel(char * word){
+    int direction = RIGHT_DIRECTION;
+    int motorNumber = 1;
+
+    moveMotor(motorNumber, direction);
+}
+
+void BarrelArranger::moveMotor(int motorNumber, int direction) {
+    if(_lastDirection != direction) {
+        digitalWrite(_directionPin, direction);
+    }
+
+    _lastDirection = direction;
+
+    for (int side = 0; side < STEPS_PER_MOVE; side++) {
+        _multiplexorHandler.sendMultiplexorData(MULTIPLEXOR_DATA_BY_MOTOR_NUMBER[motorNumber]);
+    }
+
+    if(direction == LEFT_DIRECTION) {
+        digitalWrite(_directionPin, RIGHT_DIRECTION);
+    }
+}
---
+++ BarrelArranger.h
@@ -0,0 +1,28 @@
+/*
+  BarrelArranger.h - Library for manualy moving an specific to the right or to the left
+  Created by Blindle, 22 Sept, 2017.
+  Released into the public domain.
+*/
+
+#ifndef BarrelArranger_h
+#define BarrelArranger_h
+
+#include "BarrelArranger.h"
+#include "MultiplexorHandler.h"
+
+class BarrelArranger
+{
+public:
+    BarrelArranger(MultiplexorHandler& _multiplexorHandler, int directionPin);
+    void moveBarrel(char *word);
+    bool hasToMoveBarrel(char * word);
+
+private:
+    void moveMotor(int motorNumber, int direction);
+
+    MultiplexorHandler _multiplexorHandler;
+    int _directionPin;
+    int _lastDirection;
+};
+
+#endif
