fcbd5c0b60632a2e7332e89b84644d091b8bb2e0
Emanuel Suriano <emanuel.suriano@intive-fdv.com>
2017-09-26 23:54:35 -0300

    Changing pins

--- Arduino.ino
+++ Arduino.ino
@@ -1,7 +1,8 @@
-const int DIRECTION_PIN = 11; ////Pin that control the direction
-const int LATCH_PIN = 9;  //Pin connected to ST_CP of 74HC595
-const int CLOCK_PIN = 10; //Pin connected to SH_CP of 74HC595
-const int DATA_PIN = 8;  ////Pin connected to DS of 74HC595
+// PINS_CONNECIONS
+int DIRECTION_PIN = 11; // Pin that controls the direction, connected to Pololu
+int CLOCK_PIN = 10;     // Pin connected to SH_CP of 74HC595
+int LATCH_PIN = 9;      // Pin connected to ST_CP of 74HC595
+int DATA_PIN = 8;       // Pin connected to DS of 74HC595
 
 const int RIGHT_DIRECTION = HIGH;
 const int LEFT_DIRECTION = LOW;
@@ -33,7 +34,6 @@ void loop()
 void sendDataToMultiplexors(byte first, byte second, int delayTime){
     digitalWrite(LATCH_PIN, 0);
     shiftOut(DATA_PIN, CLOCK_PIN, first);
-    shiftOut(DATA_PIN, CLOCK_PIN, second);
     digitalWrite(LATCH_PIN, 1);
     delay(delayTime);
 }
